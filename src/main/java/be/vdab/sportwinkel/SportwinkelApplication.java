bc5bd09b494cb805f87ac06390a6a8fc872c38ab
ItzelMaldonado <itzel.mn@gmail.com>
2024-08-01 13:18:35 +0200

    Creatie van het project voor RabbitMQ

---
+++ src/main/java/be/vdab/sportwinkel/SportwinkelApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.sportwinkel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportwinkelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportwinkelApplication.class, args);
+    }
+
+}
